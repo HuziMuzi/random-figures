4198d8400cd665d1846ba2a720f26535c7214da1
@andrey.davidovich <laif17860@gmail.com>
2023-04-05 09:13:18 +0300

    feat: AppButton component

---
+++ src/components/AppButton/AppButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from "react-native";
+
+type AppButtonPropsType = {
+    title: string
+    onPress: () => void
+}
+
+export const AppButton = ({title, onPress}: AppButtonPropsType) => {
+    return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFD700',
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        borderRadius: 17,
+    },
+    title: {
+        fontSize: 18,
+    }
+})
